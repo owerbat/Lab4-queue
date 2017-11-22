524f55b0c1ea9092cf45a6c72604c31e965b974a
owerbat <niktimak@gmail.com>
2017-11-22 09:25:58 +0300

    The first commit: TQueue.h and main.cpp were added

---
+++ Queue lab/Queue lab/TQueue.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned int uint;
+
+template <class T>
+class TQueue {
+	uint first;
+	uint last;
+	uint size;
+	uint maxsize;
+	T* ring;
+public:
+
+};
---
+++ Queue lab/Queue lab/main.cpp
@@ -0,0 +1,6 @@
+#include "TQueue.h"
+
+int main() {
+
+	return 0;
+}
